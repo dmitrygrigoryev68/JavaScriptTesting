3b101489b9367ab53a2f1979db4b8a3422d95a7c
Dmitry Grigoryev <dmitry.grigoryev@gmx.de>
2019-01-08 16:26:09 +0100

    first commit

---
+++ 001_Simple/hello.js
@@ -0,0 +1,9 @@
+function User(name){
+    this.name = name;
+    this.sayHello = () => {
+        console.log('Hello',this.name);
+    };
+
+}
+var John = new User('John');
+John.sayHello(); 
---
+++ 003_Variable/mod.js
@@ -0,0 +1,4 @@
+var x = 10;
+ function test(){
+     console.log(x);
+ };
